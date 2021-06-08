5934844701f584f5d81a3f775233979bab78bd4b
404kacper <404kacper>
2021-06-08 15:15:27 +0200

    Updated gradle.build for JUnit testing

---
+++ src/test/java/agentSim/basicTest.java
@@ -0,0 +1,11 @@
+package agentSim;
+
+//import org.junit.jupiter.api.Test;
+
+public class basicTest {
+//    Empty class just to upload structure to git
+//    @Test
+    public void test() {
+        System.out.println("This test should run");
+    }
+}
--- src/test/java/simTests/basicTest.java
+++
@@ -1,5 +0,0 @@
-package simTests;
-
-public class basicTest {
-//    Empty class just to upload structure to git
-}
