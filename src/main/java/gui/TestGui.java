60b708565862a4db498bba1ce1f3f54c76c86c5a
404kacper <404kacper>
2021-06-12 21:42:07 +0200

    Added scene builder and simple fxml view

--- src/main/java/gui/TestGui.java
+++ src/main/java/gui/TestGui.java
@@ -2,26 +2,37 @@ package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TestGui extends Application {
 
+//    Gui must be ran with gradle run task
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.RED);
+    public void start(Stage stage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/scene1.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("Demo app");
+
+        Image icon = new Image("https://image.flaticon.com/icons/png/512/1909/1909848.png");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
