16010551b18e79f9d2a73c1ec225b2a2ba0e3564
404kacper <404kacper>
2021-06-13 00:18:28 +0200

    Added simple step progression into simulation gui

--- src/main/java/agentSim/Simulation.java
+++ src/main/java/agentSim/Simulation.java
@@ -1,31 +1,31 @@
 package agentSim;
 
 import agentSim.agent.IAgent;
-import agentSim.agent.creator.AgentCreator;
 import agentSim.agent.creator.IAgentCreator;
 import agentSim.agent.man.Medic;
 import agentSim.map.IMap;
 import agentSim.map.creator.IMapCreator;
-import agentSim.map.creator.MapCreator;
-import gui.AppGui;
 
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Simulation {
+
     private IMap map;
     private Random rnd;
     private int maxIter;
+    private int currentIteration;
     private List<IAgent> agentList;
 
 //    Implement IAgentCreator once agents are added to the project structure
     public Simulation(IMapCreator mapCreator, IAgentCreator agentCreator, long seed, int maxIter){
     map = mapCreator.createMap();
-
     rnd=new Random(seed);
+
     initializeSimulation(agentCreator, map, rnd);
 
+    this.currentIteration = maxIter;
     this.maxIter=maxIter;
     }
 
@@ -86,7 +86,7 @@ public class Simulation {
                 }
             }
             for (IAgent agent : agentList) {
-                agent.move();
+//                agent.move();
             }
 //            Print out map after each iteration
             System.out.println("\n");
@@ -95,64 +95,75 @@ public class Simulation {
         }
     }
 
-    public void runSimulation() {
-        int iterations = maxIter;
+    public void runSimulationStep() {
+        System.out.println("Before : \n" + map);
+        if (currentIteration > 0) {
+            recoverAgents(currentIteration);
+            interact();
+            moveAgents();
+            --currentIteration;
+            System.out.println("After : \n" + map);
+        }
+    }
 
-//        Print initial state of the map
-        System.out.println("Iterations left: " + iterations);
-        System.out.println(map.toString());
-
-        while (--iterations>0) {
-//            Recover agents and remove infected - dead agents
-            Iterator <IAgent> listIterator = agentList.iterator();
-            while (listIterator.hasNext()){
-                IAgent agent = listIterator.next();
-                double genProb = ThreadLocalRandom.current().nextDouble();
+    public void recoverAgents(int iterations) {
+        //            Recover agents and remove infected - dead agents
+        Iterator <IAgent> listIterator = agentList.iterator();
+        while (listIterator.hasNext()){
+            IAgent agent = listIterator.next();
+            double genProb = ThreadLocalRandom.current().nextDouble();
 //                Initial iteration is omitted to allow agents to interact with each other
-                if (iterations != maxIter-1) {
-                    if (genProb <= agent.getDeathProb()) {
-                        if (agent.recover()) {
-                            map.removeAgent(agent);
-                            listIterator.remove();
-                        }
-                    } else {
-                        agent.recover();
+            if (iterations != maxIter) {
+                if (genProb <= agent.getDeathProb()) {
+                    if (agent.recover()) {
+                        map.removeAgent(agent);
+                        listIterator.remove();
                     }
+                } else {
+                    agent.recover();
                 }
             }
-            for (IAgent agent : agentList) {
-//                Execute infect only for infected agents
-                if (agent.getHealth() == 1) {
-//                    Catch the exception defined in medic class
-                    try {
-                        agent.infect();
-                        Collections.sort(agentList, new SortByHealth());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
-//                Execute vaccinate only for medics
-                if (agent instanceof Medic) {
-                    ((Medic) agent).vaccinate(1,2);
-                }
-            }
-            for (IAgent agent : agentList) {
-                agent.move();
-            }
-//            Print out map after each iteration
-            System.out.println("\n");
-            System.out.println("Iterations left: " + iterations);
-            System.out.println(map.toString());
         }
     }
 
-    public static void main(String[] args) {
-        MapCreator currentMap = new MapCreator(4, 4);
+    public void interact() {
+        for (IAgent agent : agentList) {
+//                Execute infect only for infected agents
+            if (agent.getHealth() == 1) {
+//                    Catch the exception defined in medic class
+                try {
+                    agent.infect();
+                    Collections.sort(agentList, new SortByHealth());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-        IAgentCreator currentAgents = new AgentCreator(1,1,1,1,1,1);
-
-        Simulation sim = new Simulation(currentMap, currentAgents,54, 10);
-        sim.runSimulation();
+            }
+//                Execute vaccinate only for medics
+            if (agent instanceof Medic) {
+                ((Medic) agent).vaccinate(1,2);
+            }
+        }
     }
+
+    public void moveAgents() {
+        for (IAgent agent : agentList) {
+            agent.move();
+        }
+    }
+
+    public IMap getSimulationMap() {
+        return map;
+    }
+
+
+
+//    public static void main(String[] args) {
+//        MapCreator currentMap = new MapCreator(3, 3);
+//
+//        IAgentCreator currentAgents = new AgentCreator(8,1,0,9,0,0);
+//
+//        Simulation sim = new Simulation(currentMap, currentAgents,54, 2);
+////        sim.runSimulationOld();
+//    }
 }
--- src/main/java/gui/MainView.java
+++ src/main/java/gui/MainView.java
@@ -1,6 +1,9 @@
 package gui;
 
 import agentSim.Simulation;
+import agentSim.agent.IAgent;
+import agentSim.agent.creator.AgentCreator;
+import agentSim.agent.creator.IAgentCreator;
 import agentSim.map.IMap;
 import agentSim.map.creator.MapCreator;
 import javafx.scene.canvas.Canvas;
@@ -14,18 +17,27 @@ import javafx.scene.transform.Affine;
 public class MainView extends VBox {
     private Button stepButton;
     private Canvas canvas;
-
     private Affine affine;
-
-    private MapCreator mapCreator;
     private IMap simulationMap;
+    private Simulation simulation;
 
     public MainView() {
+        MapCreator currentMap = new MapCreator(10, 10);
+
+        IAgentCreator currentAgents = new AgentCreator(8,1,0,9,0,0);
+
+        Simulation sim = new Simulation(currentMap, currentAgents,54, 2);
+
+        simulation = sim;
+
+        simulationMap = sim.getSimulationMap();
+
         this.stepButton = new Button("step");
         this.stepButton.setOnAction(actionEvent -> {
 //            To be implemented tomorrow
 //            Increments simulation iteration by one
-//            Simulation.step();
+            simulation.runSimulationStep();
+            draw();
 
         });
         this.canvas = new Canvas(400,400);
@@ -43,14 +55,13 @@ public class MainView extends VBox {
         g.setFill(Color.LIGHTGRAY);
         g.fillRect(0,0,400,400);
 
-        MapCreator currentMap = new MapCreator(10, 10);
-
-        simulationMap = currentMap.createMap();
-
         g.setFill(Color.BLACK);
         for (int x = 0; x < this.simulationMap.getXDim(); x++) {
             for (int y = 0; y < this.simulationMap.getYDim(); y++) {
-//                g.fillRect(x,y, 1,1);
+                IAgent currAgent = simulationMap.getAgent(x,y);
+                if (currAgent!=null) {
+                    g.fillRect(y,x, 1,1);
+                }
             }
         }
 
